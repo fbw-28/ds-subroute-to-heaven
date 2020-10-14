d77621e9707f49358ad3298ccaaaf3a7af4d864c
LenaIT <olena.denysova@gmail.com>
2020-10-14 13:52:14 +0200

    ex4 done without bonuses

---
+++ routes/skills.js
@@ -0,0 +1,75 @@
+const express = require('express')
+const router = express.Router()
+
+
+let skills = [{
+        id: '1',
+        title: 'Coding'
+    },
+    {
+        id: '2',
+        title: 'Complaining'
+    },
+    {
+        id: '3',
+        title: 'Procrastinating'
+    },
+    {
+        id: '4',
+        title: 'Improving'
+    },
+    {
+        id: '5',
+        title: 'Never giving up'
+    },
+]
+
+// routes for managing skills...
+
+router.get('/', (req, res) => {
+    res.send(skills)
+});
+
+router.get('/:id', (req, res) => {
+    const {
+        id
+    } = req.params
+    let skill = skills.find(skill => skill.id == id)
+    res.send(skill)
+})
+
+router.post('/', (req, res) => {
+    if (!req.body.title) {
+        return res.send({
+            error: "Please provide a title field!"
+        })
+    }
+    let skillNew = {
+        id: Date.now().toString(),
+        ...req.body
+    }
+    skills.push(skillNew)
+    res.send(skillNew)
+});
+
+router.patch(' /:id', (req, res) => {
+    const {
+        id
+    } = req.params
+    let skillToUpdate = skills.find(skill => skill.id == id)
+    Object.assign(skillToUpdate, {
+        ...req.body
+    })
+    res.send(skillToUpdate)
+})
+
+router.delete('/:id', (req, res) => {
+    const {
+        id
+    } = req.params
+    let skill = skills.find(skill => skill.id == id) // find item to delete
+    skills = skills.filter(skill => skill.id != id) // delete item by filtering it out
+    res.send(skill) // return deleted item
+})
+
+module.exports = router
---
+++ routes/todos.js
@@ -0,0 +1,66 @@
+const express = require('express')
+const router = express.Router()
+
+let todos = [{
+        id: '1',
+        name: 'Wake the cat',
+        done: false
+    },
+    {
+        id: '2',
+        name: 'Make a Corona Test',
+        done: false
+    },
+    {
+        id: '3',
+        name: 'Trim off your neighbours beard',
+        done: true
+    },
+    {
+        id: '4',
+        name: 'Learn to meditate and become one with the backend',
+        done: false
+    }
+]
+
+// routes for managing TODOS...
+
+router.get('/', (req, res) => {
+    res.send(todos)
+});
+
+router.get('/:id', (req, res) => {
+    const {id} = req.params
+    let todo = todos.find(todo => todo.id == id)
+    res.send(todo)
+})
+
+router.post('/', (req, res) => {
+    if (!req.body.name) {
+        return res.send({
+            error: "Please provide a name field!"
+        })
+    }
+    let todoNew = {
+        id: Date.now().toString(),
+        ...req.body
+    }
+    todos.push(todoNew)
+    res.send(todoNew)
+});
+
+router.patch('/:id', (req, res) => {
+    const {id} = req.params
+    let todo = todos.find(todo => todo.id == id)
+    Object.assign(todo, {...req.body})
+    res.send(todo)
+})
+
+router.delete('/:id', (req, res) => {
+    const {id} = req.params
+    let todo = todos.find(todo => todo.id == id) // find item to delete
+    todos = todos.filter(todo => todo.id != id) // delete item by filtering it out
+    res.send(todo) // return deleted item
+})
+
+module.exports = router
