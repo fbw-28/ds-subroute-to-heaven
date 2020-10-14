440711b49877b69a9ed09a368c032f5cfccfa42e
Filippo <filippo.moretti6@gmail.com>
2020-10-14 18:39:57 +0200

    without Lowdb

--- server.js
+++ server.js
@@ -1,141 +1,95 @@
 const express = require('express');
 const app = express();
+const port= 5000
 
-// After server started, visit http://localhost:5000 in a browser to see the output.
+app.use( express.json() ) // => req.body
+
+let fruits = [
+  { id: "1", name: 'Banana', color: 'yellow'},
+  { id: "2", name: 'Cherry', color: 'red'},
+  { id: "3", name: 'Lemon', color: 'yellow'},
+  { id: "4", name: 'Apple', color: 'green'},
+  { id: "5", name: 'Orange', color: 'orange'}
+]
+
+app.get('/fruits', (req, res) => {
+  res.json(fruits)
+});
+
+//GET
+app.get('/fruits/:id', (req, res) => {
+  console.log(req.params)
+
+  //fruiFound is gonna be an object
+  let fruitFound = fruits.find(fruit => fruit.id == req.params.id )
+
+  if(!fruitFound){
+    return res.json({message: `we don't have any ${req.params.id}`})
+  }else{
+    return res.json({message:`we have the ${fruitFound.name} and its color is ${fruitFound.color} ?`})}
+})
+
+//POST
+app.post('/newfruit', (req, res) => {
+  let fruitNew = req.body
+  fruits.push(fruitNew)
+  res.json(fruitNew)
+})
+
+//DELETE
+app.delete('/fruits/:id', (req, res) => {
+ 
+/* let id= req.params.id */ //or...
+  let { id } = req.params
+  let  selectedFruit= fruits.find(fruit=>fruit.id==id)
+  fruits = fruits.filter(fruit => id != fruit.id)
+
+  //I get the full object of the specific fruit
+   
+  console.log(selectedFruit)
+  res.json({ message: `${selectedFruit.name} was deleted successfully ` })
+})
+
+//PATCH
+app.patch('/fruits/:id', (req, res) => {
+
+  let { id } = req.params
+  let fields = req.body
+
+  console.log(id, fields)
+
+  // HOW THE FUCK DO I UPDATE A FUCKING OBJECT 
+  // IN A FUCKING ARRAY?
+
+  let fruitFound = fruits.find(fruit => fruit.id === id)
+
+  if(fruitFound) {
+    Object.assign(fruitFound, {...req.body})
+    res.send(fruitFound)
+  }
+  else {
+    res.json({ message: 'What do you think we are here? Please provide someone we know!!!' })
+  }
+
+})
+
+//Bonus
+app.get("/colors", (req, res) => {
+  const allColors = fruits.map((item) => item.color);
+  console.log(allColors);
+  const uniqColors = [...new Set(allColors)];
+  console.log(uniqColors)
+  res.json(uniqColors);
+});
+
+app.get("/plain", (req, res) => {
+  console.log("plain");
+  const plainFruits = fruits.map((item) => item.name);
+  console.log(plainFruits);
+  res.json(plainFruits);
+});
+
 app.listen(5000, () => {
   console.log('Example app listening on port 5000!');
 });
-
-app.use(express.json()) // parse incoming bodies... we can access then the data in req.body everywhere
-
-let todos = [
-  { id: '1', name: 'Wake the cat', done: false },
-  { id: '2', name: 'Make a Corona Test', done: false },
-  { id: '3', name: 'Trim off your neighbours beard', done: true },
-  { id: '4', name: 'Learn to meditate and become one with the backend', done: false }
-]
-
-let skills = [
-  { id: '1', title: 'Coding'}, 
-  { id: '2', title: 'Complaining'}, 
-  { id: '3', title: 'Procrastinating'}, 
-  { id: '4', title: 'Improving'}, 
-  { id: '5', title: 'Never giving up'}, 
-]
-
-let users = [
-  { id: '1', username: 'user1', password: 'pw1'},
-  { id: '2', username: 'user2', password: 'pw2'},
-  { id: '3', username: 'user3', password: 'pw3'},
-]
-
-// routes for managing TODOS...
-  // move us away from here please...
-
-app.get('/todos', (req, res) => {
-  res.send(todos)
-});
-
-app.get('/todos/:id', (req, res) => {
-  const { id } = req.params
-  let todo = todos.find(todo => todo.id == id)
-  res.send(todo)
-})
-
-app.post('/todos', (req, res) => {
-  if(!req.body.name) {
-    return res.send({error: "Please provide a name field!"})
-  }
-  let todoNew = { id: Date.now().toString(), ...req.body }
-  todos.push(todoNew)
-  res.send(todoNew)
-});
-
-app.patch('/todos/:id', (req, res) => {
-  const { id } = req.params
-  let todo = todos.find(todo => todo.id == id)
-  Object.assign(todo, {...req.body})
-  res.send(todo)
-})
-
-app.delete('/todos/:id', (req, res) => {
-  const { id } = req.params
-  let todo = todos.find(todo => todo.id == id) // find item to delete
-  todos = todos.filter(todo => todo.id != id) // delete item by filtering it out
-  res.send(todo) // return deleted item
-})
-
-
-// routes for managing skills...
-  // move us away from here please...
-
-app.get('/skills', (req, res) => {
-  res.send(skills)
-});
-
-app.get('/skills/:id', (req, res) => {
-  const { id } = req.params
-  let skill = skills.find(skill => skill.id == id)
-  res.send(skill)
-})
-
-app.post('/skills', (req, res) => {
-  if(!req.body.title) {
-    return res.send({error: "Please provide a title field!"})
-  }
-  let skillNew = { id: Date.now().toString(), ...req.body }
-  skills.push(skillNew)
-  res.send(skillNew)
-});
-
-app.patch('/skills/:id', (req, res) => {
-  const { id } = req.params
-  let skillToUpdate = skills.find(skill => skill.id == id)
-  Object.assign(skillToUpdate, {...req.body})
-  res.send(skillToUpdate)
-})
-
-app.delete('/skills/:id', (req, res) => {
-  const { id } = req.params
-  let skill = skills.find(skill => skill.id == id) // find item to delete
-  skills = skills.filter(skill => skill.id != id) // delete item by filtering it out
-  res.send(skill) // return deleted item
-})
-
-
-// routes for managing users...
-  // move us away from here please...
-
-app.get('/users', (req, res) => {
-  res.send(users)
-});
-
-app.get('/users/:id', (req, res) => {
-  const { id } = req.params
-  let user = users.find(user => user.id == id)
-  res.send(user)
-})
-
-app.post('/users', (req, res) => {
-  if(!req.body.username) {
-    return res.send({error: "Please provide a username field!"})
-  }
-  let userNew = { id: Date.now().toString(), ...req.body }
-  users.push(userNew)
-  res.send(userNew)
-});
-
-app.patch('/users/:id', (req, res) => {
-  const { id } = req.params
-  let userToUpdate = users.find(user => user.id == id)
-  Object.assign(userToUpdate, {...req.body})
-  res.send(userToUpdate)
-})
-
-app.delete('/users/:id', (req, res) => {
-  const { id } = req.params
-  let user = users.find(user => user.id == id) // find item to delete
-  users = users.filter(user => user.id != id) // delete item by filtering it out
-  res.send(user) // return deleted item
-})
-
+//Run app, then load http://localhost:5000 in a browser to see the output.
